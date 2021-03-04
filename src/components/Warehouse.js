1273d4b595bd4c7ec337e27a5ad00a1404000959
Sayantan Mishra <sayanmdn@gmail.com>
2021-03-04 20:48:04 +0530

    warehouse to be constructed add

--- src/components/Warehouse.js
+++ src/components/Warehouse.js
@@ -1,53 +1,66 @@
-import React, { useState, useEffect } from 'react'
-import { Loginform } from "./forms/LoginForm";
-import axios from 'axios'
-import {URL} from '../config'
-import { useSelector, useDispatch } from "react-redux";
-
+import React from 'react'
 
 export function Warehouse(props) {
-var [isValidToken, setIsValidToken] = useState(null)
-const auth = useSelector(state => state.auth)
+    
 
-
-// console.log("Token is "+authToken)
-useEffect(()=>{
-    console.log("Useeffect using:221  "+ auth.user)
-    if(!auth.isLoggedIn){
-        const authToken = localStorage.getItem('token')
-        axios.post(`${URL}post/isAuthenticated`, {"token": authToken})
-        .then (res =>{
-            console.log(res)
-            if(res.data.code === "tokenValid"){
-                console.log("res.data.code out: "+res.data.code)
-            }
-            setIsValidToken(res.data) 
-        })
-        setIsValidToken(true)
-    }
-},[auth])
-const removeToken = () =>{
-    localStorage.setItem('token', null)
-    setIsValidToken(null)
+    return (
+        <>
+            <h2>To be constructed</h2>
+        </>
+    )
 }
 
-console.log("Valid token out: "+isValidToken)
 
-    if(!isValidToken) {return (
-        <div style={{background:"linear-gradient(#112233, #002222)", color:"white", textAlign:"center", height:"93vh"}}>
-            <div style={{paddingTop:"10vh"}}></div>
-                <Loginform/>
-        </div>
-    )} else {
-        return(
-            <div style={{background:"linear-gradient(#112233, #002222)", color:"white", textAlign:"center", height:"93vh"}}>
-            <div style={{paddingTop:"10vh"}}></div>
-                <h2>Hi, </h2>
-                <h2>You are already loggedin</h2>
-                <button onClick={removeToken}>Logout</button>
-        </div>
-        )
-    }
-}
+// import React, { useState, useEffect } from 'react'
+// import { Loginform } from "./forms/LoginForm";
+// import axios from 'axios'
+// import {URL} from '../config'
+// import { useSelector, useDispatch } from "react-redux";
+
+
+// export function Warehouse(props) {
+// var [isValidToken, setIsValidToken] = useState(null)
+// const auth = useSelector(state => state.auth)
+
+
+// // console.log("Token is "+authToken)
+// useEffect(()=>{
+//     console.log("Useeffect using:221  "+ auth.user)
+//     if(!auth.isLoggedIn){
+//         const authToken = localStorage.getItem('token')
+//         axios.post(`${URL}post/isAuthenticated`, {"token": authToken})
+//         .then (res =>{
+//             console.log(res)
+//             if(res.data.code === "tokenValid"){
+//                 console.log("res.data.code out: "+res.data.code)
+//             }
+//             setIsValidToken(res.data) 
+//         })
+//         setIsValidToken(true)
+//     }
+// },[auth])
+// const removeToken = () =>{
+//     localStorage.setItem('token', null)
+//     setIsValidToken(null)
+// }
+
+// console.log("Valid token out: "+isValidToken)
+
+//     if(!isValidToken) {return (
+//         <div style={{background:"linear-gradient(#112233, #002222)", color:"white", textAlign:"center", height:"93vh"}}>
+//             <div style={{paddingTop:"10vh"}}></div>
+//                 <Loginform/>
+//         </div>
+//     )} else {
+//         return(
+//             <div style={{background:"linear-gradient(#112233, #002222)", color:"white", textAlign:"center", height:"93vh"}}>
+//             <div style={{paddingTop:"10vh"}}></div>
+//                 <h2>Hi, </h2>
+//                 <h2>You are already loggedin</h2>
+//                 <button onClick={removeToken}>Logout</button>
+//         </div>
+//         )
+//     }
+// }
 
 
