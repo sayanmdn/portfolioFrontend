a4c163a4f2a476b5d36992a7d0e386cd3f1fa281
Sayantan Mishra <sayanmdn@gmail.com>
2021-03-12 15:35:44 +0530

    api subdomain add

--- src/config.js
+++ src/config.js
@@ -1,1 +1,4 @@
-export const URL = "https://stan.sumankundu.com/"
+// export const URL = "https://stan.sumankundu.com/"
+export const URL = "https://api.sayantanmishra.com/"
+
+//api.sayantanmishra.com
