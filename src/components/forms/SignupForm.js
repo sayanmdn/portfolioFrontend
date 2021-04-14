c50e94918f9ddde31d17772c435a00d27d8b5821
Sayantan Mishra <sayanmdn@gmail.com>
2021-04-15 04:53:22 +0530

    otp add

--- src/components/forms/SignupForm.js
+++ src/components/forms/SignupForm.js
@@ -9,6 +9,7 @@ export function SignupForm(props) {
     var [emailAlreadyExists, setemailAlreadyExists] = useState(false)
     var [signupSuccess, setSignupSuccess] = useState(false)
     var [passwordValidationError, setPasswordValidationError] = useState(null)
+    const [otpSentSuccessfully, setOtpSentSuccessfully] = useState(false)
 
         // Pass the useFormik() hook initial form values and a submit function that will
         // be called when the form is submitted
@@ -16,7 +17,8 @@ export function SignupForm(props) {
           initialValues: {
                 email: '',
                 name:'',
-                password:''
+                password:'',
+                otp:''
           },
           onSubmit: values => {
             axios.post(`${URL}user/signup`, values)
@@ -47,6 +49,19 @@ export function SignupForm(props) {
             // alert(JSON.stringify(values));
           },
         });
+        const sendOTP = ()=>{
+            let email = formik.values.email
+            console.log("Email provided to send otp "+email)
+            axios.post(`${URL}user/otpsend`, {email})
+            .then(res=>{
+                console.log("Email Sent response 1 "+JSON.stringify(res))
+                if(res.data.code == "otpSent"){
+                    console.log("Email Sent response 2 "+res)
+                    setOtpSentSuccessfully(true)
+                    alert("OTP Sent Successfully to your Email")
+                }
+            })
+        }
     if(signupSuccess) return (
         <div>
         <h2>Signup Successful</h2>
@@ -59,13 +74,21 @@ export function SignupForm(props) {
             <Form.Group controlId="formBasicName">
                 <Form.Label>Name</Form.Label>
                 <Form.Control type="name" name="name" placeholder="Enter name" onChange={formik.handleChange} value={formik.values.name}/>
-                <Form.Text className="text-muted" hidden={!emailAlreadyExists}>
-                This email is already exists, try with another email
-                </Form.Text>
             </Form.Group>
             <Form.Group controlId="formBasicEmail">
                 <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" name="email" onChange={formik.handleChange} value={formik.values.email} />
+                <Form.Control type="email" placeholder="Enter email" name="email" readOnly={otpSentSuccessfully} onChange={formik.handleChange} value={formik.values.email} />
+                <Form.Text className="text-muted" hidden={!emailAlreadyExists}>
+                This email is already exists, try with another email
+                </Form.Text>
+                <Form.Text className="text-muted">
+                <Button onClick={sendOTP} hidden={otpSentSuccessfully}>Send OTP to my email</Button>
+                </Form.Text>
+            </Form.Group>
+            <Form.Group controlId="formBasicOTP">
+                <Form.Label>OTP</Form.Label>
+                <Form.Control type="otp" name="otp" placeholder="Enter otp" onChange={formik.handleChange} value={formik.values.otp}/>
+                
             </Form.Group>
 
             <Form.Group controlId="formBasicPassword">
