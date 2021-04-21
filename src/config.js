cf9c37b9d059132451c87fc407bf4a83968ec1f8
Sayantan Mishra <sayanmdn@gmail.com>
2021-04-21 19:07:44 +0530

    api ura clange

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 // export const URL = "https://stan.sumankundu.com/"
-// export const URL = "https://api.sayantanmishra.com/"
-export const URL = "http://localhost:8000/"
+export const URL = "https://api.sayantanmishra.com/"
+// export const URL = "http://localhost:8000/"
 
 //api.sayantanmishra.com
