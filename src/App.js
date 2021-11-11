3712076a60504b426c0b08a3f523568da9bf3c36
Sayantan Mishra <sayanmdn@gmail.com>
2021-11-11 15:01:57 +0530

    analytics add

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import "./App.css";
-import {useEffect} from 'react'
+import { useEffect } from "react";
 import { Navigationbar } from "./components/Navigationbar";
 import { Login } from "./components/Login";
 import { Area1 } from "./components/Area1";
 import { Signup } from "./components/Signup";
 import { Warehouse } from "./components/Warehouse";
 import { Cake } from "./components/CakeContainer";
-import axios from 'axios'
-import {useDispatch} from "react-redux"
-
+import axios from "axios";
+import { useDispatch } from "react-redux";
+import reactGa from "react-ga";
 import { delAuth, initAuth } from "./redux/actions";
 
 import { Provider } from "react-redux";
@@ -16,11 +16,12 @@ import { Provider } from "react-redux";
 import store from "./redux/store";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-
 function App() {
   useEffect(() => {
-    document.title = "Sayantan Mishra | Software Developer"
- }, []);
+    document.title = "Sayantan Mishra | Software Developer";
+    reactGa.initialize("UA-92548969-2");
+    reactGa.pageview("/");
+  }, []);
 
   return (
     <Provider store={store}>
