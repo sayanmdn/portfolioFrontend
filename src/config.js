0538cfe401e5d445ecea052d9f3b7cb3ee663b24
Sayantan Mishra <sayanmdn@gmail.com>
2022-05-25 15:03:53 +0530

    backend endpoint change

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 // export const URL = "https://stan.sumankundu.com/"
-export const URL = "https://api.sayantanmishra.com/"
-// export const URL = "http://localhost:8000/"
-
-//api.sayantanmishra.com
+// export const URL = "https://api.sayantanmishra.com/";
+export const URL =
+  "https://r9hajslp5h.execute-api.ap-southeast-1.amazonaws.com/dev/";
+// export const URL = "http://localhost:8080/"
