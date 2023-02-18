59ac8fbebabd62d8e6a527fa1a75cb895e4560df
Sayantan Mishra <sayanmdn@gmail.com>
2023-02-18 17:57:56 +0530

    config change

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 // export const URL = "https://stan.sumankundu.com/"
 // export const URL = "https://api.sayantanmishra.com/";
 export const URL =
-  "https://r9hajslp5h.execute-api.ap-southeast-1.amazonaws.com/dev/";
-// export const URL = "http://localhost:8080/"
+  "https://n7q2kpecfc.execute-api.ap-southeast-1.amazonaws.com/dev/";
+// "https://r9hajslp5h.execute-api.ap-southeast-1.amazonaws.com/dev/";
+// export const URL = "http://localhost:8080/";
