a3aed7c8ad89e0c1d3078130ab5b81e3907ca86c
Sayantan Mishra <sayanmdn@gmail.com>
2024-04-04 00:20:57 +0530

    backend endpoint update

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 // export const URL =
 //   "https://n7q2kpecfc.execute-api.ap-southeast-1.amazonaws.com/dev/";
 // export const URL = "http://localhost:8080/";
-export const URL =
-  "https://fy5f66hx18.execute-api.ap-south-1.amazonaws.com/dev/";
+export const URL = "https://api2.sayantanmishra.com/";
+// export const URL =
+//   "https://fy5f66hx18.execute-api.ap-south-1.amazonaws.com/dev/";
